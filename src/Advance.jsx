c2bddf1087075744afc840ab0e58862345f1e31e
Moginder <mogiegan@gmail.com>
2021-12-02 23:35:25 +0530

    Update Advance.jsx

--- src/Advance.jsx
+++ src/Advance.jsx
@@ -1,4 +1,28 @@
 import React, { useState } from 'react'
+import Card from '@material-ui/core/Card'
+import DeleteIcon from '@material-ui/icons/Delete';
+import AddIcon from '@material-ui/icons/Add';
+import Container from '@material-ui/core/Container';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import RemoveIcon from '@material-ui/icons/Remove';
+import Icon from '@material-ui/core/Icon';
+import { v4 as uuidv4 } from 'uuid';
+import {Row, Col } from 'react-grid-system';
+
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    '& .MuiTextField-root': {
+      margin: theme.spacing(1),
+    },
+  },
+  button: {
+    margin: theme.spacing(1),
+  }
+}))
 
 const Advance = () => {
     // for card
@@ -10,23 +34,9 @@ const Advance = () => {
     const [clutcher  , setclutcher] = useState(false)
     const [sniper  , setsniper] = useState(false)
     
-    const[ Showrank , setshowrank ]=useState(false)
+    const[ showrank , setshowrank ]=useState(false)
     const [showrange  , setshowrange] = useState(false)
-    const [data , setdata]= useState({
-        rank:"",
-        prize:""
-    })
-
-    const [rangedata  , setrangedata ]=useState({
-        rangeto:"",
-        rangefrom:"",
-        prize:""
-
-    })
-
-
-    const [printdata , setprintdata]=useState([])
-    const [printrange , setprintrange] = useState([])
+  
     
     const [mode , setmode] = useState("")
 
@@ -40,141 +50,168 @@ const Advance = () => {
       top_sniper:""
     })
         
-    const eventHandle = (e)=>{
-        const value = e.target.value;
-        const name = e.target.name;
-
-        setdata({...data,[name]:value})  
-        
-    }
-
-    const eventHandle2 = (e)=>{
-        const value = e.target.value;
-        const name = e.target.name;
-
-        setrangedata({...rangedata,[name]:value})  
-    }
-
-
     const eventHandle3 = (e)=>{
       const value = e.target.value;
       const name = e.target.name;
 
-      setadvdata({...rangedata,[name]:value})  
+      setadvdata({...rangeFields,[name]:value})  
   }
 
+  const [inputFields, setInputFields] = useState([
+    { id: uuidv4(), rank: '', prize: '' },
+  ]);
 
-    const sendData = ()=>{
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("InputFields", inputFields);
+  };
 
-        if(!data){
-         alert("please fill the data")
-        }else{
+  const handleChangeInput = (id, event) => {
+    const newInputFields = inputFields.map(i => {
+      if(id === i.id) {
+        i[event.target.name] = event.target.value
+      }
+      return i;
+    })
+    
+    setInputFields(newInputFields);
+  }
 
-            setprintdata((olditem)=>{
-                return [...olditem , data]
-            })
-        }
+  const handleAddFields = () => {
+    setInputFields([...inputFields, { id: uuidv4(),  rank: '', prize: '' }])
+  }
 
-        setdata("")
-    }
+  const handleRemoveFields = id => {
+    const values  = [...inputFields];
+    values.splice(values.findIndex(value => value.id === id), 1);
+    setInputFields(values);
+  }
+  
+/*Range*/
 
-    const sendData2 = ()=>{
+  const [rangeFields , setRangefields ]=useState([{
+    id: uuidv4(),
+    rangeto:"",
+    rangefrom:"",
+    prize:""
 
-        if(!rangedata){
-         alert("please fill the data")
-        }else{
+}])
 
-            setprintrange((olditem)=>{
-                return [...olditem , rangedata]
-            })
-        }
+const handlerSubmit = (e) => {
+    e.preventDefault();
+    console.log("RangeFields", rangeFields);
+  };
+  
+  const handlerChangeInput = (id, event) => {
+    const newRangeFields = rangeFields.map(i => {
+      if(id === i.id) {
+        i[event.target.name] = event.target.value
+      }
+      return i;
+    })
+    setRangefields(newRangeFields);
+}
+const handlerAddFields = () => {
+    setRangefields([...rangeFields, { id: uuidv4(),  rangeto: '', rangefrom: '', prize:'' }])
+  }
+  const handlerRemoveFields = id => {
+    const values  = [...rangeFields];
+    values.splice(values.findIndex(value => value.id === id), 1);
+    setRangefields(values);
+  }
 
-        setrangedata("")
-    }
+   
 
 
-
-
-    const deleteItem = (index)=>{
-            console.log(index);
-         setprintdata(
-            printdata.filter((ele , i)=>{
-                return i!==index
-          })
-         )               
-    }
-
-    const deleteItem2 = (index)=>{
-        console.log(index);
-     setprintrange(
-        printrange.filter((ele , i)=>{
-            return i!==index
-      }))
 
 
      
 
                   
-}
+
     return (
-        <div>
-         <section onChange={(e)=>{setmode(e.target.value)}} >
-          <h4> Mode </h4>
-            <input type="radio" value="Squad" name="mode" /> Squad
-             <input type="radio" value="Duo" name="mode" /> Duo
-            <input type="radio" value="solo" name="mode" /> solo 
-            <br />
-        </section>
-        <br />
-    <label htmlFor=""> Add Rank & Prize Amount </label>
-    <br/>
-    <button className="Add_Rank" onClick={()=>{setshowrank(s => !s)}} > Add Rank </button>
-    <button className="Add_Range" onClick={()=>{setshowrange(s=> !s)}} > Add Range </button>
-    <br />
-    <br />
-  {/* rank ka section */}
-    <div style={{display: Showrank ? 'block' :"none"}} >
-        <input type="text" placeholder="enter your rank" name="rank" value = {data.rank} onChange={eventHandle}/>
-        <input type="text" placeholder ="enter the amount" name="prize" value={data.prize} onChange={eventHandle}/>
-        <button onClick={sendData}> +</button>
-        <br />
-        </div>
-        {/* *************************range ka secrtion***************************** */}
-        <div style={ {display:showrange? "block ":"none" }} >
-               <div className="mainrange">
-               <input type="numder" placeholder="range" name="rangeto" value={rangedata.rangeto} onChange={eventHandle2} />
-                <p>to</p>
-                <input type="numder" placeholder="range" name="rangefrom" value={rangedata.rangefrom}onChange={eventHandle2}/>
-                <input type="numder"  placeholder="prize" name="prize" value={rangedata.prize} onChange={eventHandle2}/>
-                <button onClick={sendData2} >+</button>
-               </div> 
-                 </div>
-                 {/* map method in rank */}
-        {
+        <>
+         <section onChange={(e) => { setmode(e.target.value) } }>
+                <h4> Mode </h4>
+                <input type="radio" value="Squad" name="mode" /> Squad
+                <input type="radio" value="Duo" name="mode" /> Duo
+                <input type="radio" value="solo" name="mode" /> solo
+                <br />
+            </section><br /><label htmlFor=""> Add Rank & Prize Amount </label><br /><button className="Add_Rank" onClick={() => { setshowrank(s => !s) }}> Add Rank </button><button className="Add_Range" onClick={() => { setshowrange(s => !s) }}> Add Range </button><br /><br />
+      {/* rank ka section */}
+        <div style={{display: showrank ? 'block' :"none"}} >
+        <Container>
+      <form onSubmit={handleSubmit}>
+        { inputFields.map(inputField => (
+          <div key={inputField.id}>
+            <TextField name="rank"label="Rank"variant="filled"value={inputField.rank}onChange={event => handleChangeInput(inputField.id, event)}/>
+            &nbsp;&nbsp;
+            <TextField name="prize"label="Prize"variant="filled"value={inputField.prize}onChange={event => handleChangeInput(inputField.id, event)}/>
+            <IconButton
+              onClick={handleAddFields}
+            >
+              <AddIcon />
+            </IconButton>
+            <IconButton disabled={inputFields.length === 1} onClick={() => handleRemoveFields(inputField.id)}>
+              <RemoveIcon />
+            </IconButton>
+          </div>
+        )) }
+        <Button variant="contained" color="primary" type="submit" endIcon={<Icon></Icon>}onClick={handleSubmit}>Submit</Button>
+      </form>
+    </Container>
+            </div>
+
+            {/* *************************range ka secrtion***************************** */}
+            
+            <div style={ {display: showrange ? "block ":"none" }}>
+            <Container>
+      <form onSubmit={handlerSubmit}>
+        { rangeFields.map(rangeField => (
+          <div key={rangeField.id}>
+            <TextField name="rangeto"label="Rangeto"variant="filled"value={rangeField.rangeto} onChange={event => handlerChangeInput(rangeField.id, event)}/>
+            &nbsp;&nbsp;
+            <TextField name="rangefrom"label="Rangefrom"variant="filled"value={rangeField.rangefrom}onChange={event => handlerChangeInput(rangeField.id, event)}/>
+             <TextField name="prize"label="Prize"variant="filled"value={rangeField.prize}onChange={event => handlerChangeInput(rangeField.id, event)}/>
+             <IconButton
+              onClick={handlerAddFields}
+            >
+              <AddIcon />
+            </IconButton>
+            <IconButton disabled={rangeFields.length === 1} onClick={() => handlerRemoveFields(rangeField.id)}>
+              <RemoveIcon />
+            </IconButton>
+          </div>
+        )) }
+        <Button variant="contained"  color="primary"  type="submit" endIcon={<Icon></Icon>} >Submit</Button>
+      </form>
+    </Container>
+     </div>
+             {/* map method in rank */}
+        {/*
             printdata.map((ele , index)=>{
                     return (
-                        <div>
+                        <>
                       <div className="mainrank" key = {index}>
                      <div className="rank">   <p>Rank : {ele.rank} </p>  </div>
                        <div className="prize"> <p> prize:{ele.prize}  </p></div>
                        <div className="inr"> <p>inr :{ele.prize*2}</p>   </div>
                        <button onClick={()=>{deleteItem(index)}} > <i class="zmdi zmdi-delete"></i></button>
                       </div>
-                   </div>
-
+                   </>
                     )
             })
-        }
+          */}
 
     
   
 
 
        
-        {
+        {/*
              printrange.map((ele , index)=>{
                 return (
-                    <div>
+                    <>
                   <div className="mainrank" key = {index}>
                  <div className="rank">   <p>Range : {ele.rangeto} </p>  </div>
                  <p>to</p>
@@ -184,125 +221,142 @@ const Advance = () => {
                    <button onClick={()=>{deleteItem2(index)}} > <i class="zmdi zmdi-delete"></i></button>
                    
                   </div>
-               </div>
-
+               </>
                 )
         })
-        }
+      */}
 
         {/* advance page */}
         
         <div className="mainAdv">
-            <div className="mvp">
+         <Row>
+            <Col sm={3} md={6} lg={9}>
            < input type="CheckBox"   onClick= {()=>{setmvp(s => !s)}}onClick= {()=>{setmvp(s => !s)}} />
-            <p>MVP</p>
-          <div className="card" style={ {display:mvp? "block ":"none" }}>
+           </Col>
+           <Col sm={3} md={6} lg={9}>
+            <p >MVP</p>
+            </Col>
+            <Col sm={4}>
+            
+          <div  style={ {display:mvp? "block ":"none" }}>
+          <Row>
+          <Col sm={4}>
           <input type="text" placeholder="Numder of Prizes" />
-            <input type="text" placeholder="prize amount" />
-           <div className="condition">
+          </Col>
+          <Col sm={4}>
+          <input type="text" placeholder="prize amount" />
+          </Col>
+           
              {/* radio btn section*/}
-           <section onChange={eventHandle3}>
-                  <input type="radio" value="minimum" name="mvp"/> <label htmlFor=""> Minimum</label>
-            <input type="radio" name="mvp" value="range" /> <label>range</label>
-               </section>
+           <Col sm={9}>
+                  <input type="radio" value="minimum" onChange={eventHandle3} name="mvp"/> <label htmlFor=""> Minimum</label>
+                  </Col>
+                  <Col sm={9}>
+                  <input type="radio" name="mvp" onChange={eventHandle3} value="range" /> <label>range</label>
                
-           </div>
-          </div>
+              </Col>
+               
+        
+              </Row>
             </div>
+           
+            </Col>
+            </Row>
             <div className="mvp">
            < input type="CheckBox"   onClick= {()=>{setA_kill(s => !s)}}/>
             <p>Top_Assaulter(kills)</p>
-            <div className="card" style={ {display:A_kill? "block ":"none" }}>
+            <div  style={ {display:A_kill? "block ":"none" }}>
           <input type="text" placeholder="Numder of Prizes" />
             <input type="text" placeholder="prize amount" />
-           <div className="condition">
+          
              {/* radio btn section*/}
-               <section onChange={eventHandle3}>
-                  <input type="radio" name="top_assaulter_kill" value="minimum"/> <label htmlFor=""> Minimum</label>
-            <input type="radio" name="top_assaulter_kill" value="range" /> <label>range</label>
-               </section>
-           </div>
+               
+                  <input type="radio"  onChange={eventHandle3} name="top_assaulter_kill" value="minimum"/> <label htmlFor=""> Minimum</label>
+            <input type="radio"  onChange={eventHandle3} name="top_assaulter_kill" value="range" /> <label>range</label>
+              
+           
           </div>
             </div>
             <div className="mvp">
            < input type="CheckBox"   onClick= {()=>{setA_damage(s => !s)}}/>
             <p>Top Assaulter (Damage)</p>
-            <div className="card" style={ {display:A_damage? "block ":"none" }}>
+            <div  style={ {display:A_damage? "block ":"none" }}>
           <input type="text" placeholder="Numder of Prizes" />
             <input type="text" placeholder="prize amount" />
-           <div className="condition">
+        
              {/* radio btn section*/}
-               <section onChange={eventHandle3}>
-                  <input type="radio" name="top_assaulter_damage"  value="minimum"/> <label htmlFor=""> Minimum</label>
-            <input type="radio" name="top_assaulter_damage" value="range" /> <label>range</label>
-               </section>
-           </div>
+               
+                  <input type="radio"  onChange={eventHandle3} name="top_assaulter_damage"  value="minimum"/> <label htmlFor=""> Minimum</label>
+            <input type="radio"  onChange={eventHandle3} name="top_assaulter_damage" value="range" /> <label>range</label>
+               
+          
           </div>
             </div>
             <div className="mvp">
            < input type="CheckBox"   onClick= {()=>{setReviver(s => !s)}}/>
             <p>Top Reviver</p>
-            <div className="card" style={ {display:Reviver? "block ":"none" }}>
+            <div  style={ {display:Reviver? "block ":"none" }}>
           <input type="text" placeholder="Numder of Prizes" />
             <input type="text" placeholder="prize amount" />
-           <div className="condition">
+       
              {/* radio btn section*/}
-               <section onChange={eventHandle3}>
-                  <input type="radio" name="top_reviver" value="minimum"/> <label htmlFor=""> Minimum</label>
-            <input type="radio" name="top_reviver" value="range" /> <label>range</label>
-               </section>
-           </div>
+              
+                  <input type="radio"  onChange={eventHandle3} name="top_reviver" value="minimum"/> <label htmlFor=""> Minimum</label>
+            <input type="radio"  onChange={eventHandle3} name="top_reviver" value="range" /> <label>range</label>
+               
+          
           </div>
             </div>
             <div className="mvp">
            < input type="CheckBox"   onClick= {()=>{setgreanadier(s => !s)}}/>
             <p>Top Greanadier</p>
-            <div className="card" style={ {display:greanadier? "block ":"none" }}>
+            <div  style={ {display:greanadier? "block ":"none" }}>
           <input type="text" placeholder="Numder of Prizes" />
             <input type="text" placeholder="prize amount" />
-           <div className="condition">
+        
              {/* radio btn section*/}
-               <section onChange={eventHandle3}>
-                  <input type="radio" name="top_greanadier" value="minimum"/> <label htmlFor=""> Minimum</label>
-            <input type="radio" name="top_greanadier" value="range" /> <label>range</label>
-               </section>
-           </div>
+               
+                  <input type="radio"  onChange={eventHandle3} name="top_greanadier" value="minimum"/> <label htmlFor=""> Minimum</label>
+            <input type="radio"  onChange={eventHandle3} name="top_greanadier" value="range" /> <label>range</label>
+               
+       
           </div>
             </div>
             <div className="mvp">
            < input type="CheckBox"   onClick= {()=>{setclutcher(s => !s)}}/>
             <p>Top Clutcher</p>
-            <div className="card" style={ {display:clutcher? "block ":"none" }}>
+            <div  style={ {display:clutcher? "block ":"none" }}>
           <input type="text" placeholder="Numder of Prizes" />
             <input type="text" placeholder="prize amount" />
-           <div className="condition">
+       
              {/* radio btn section*/}
-               <section onChange={eventHandle3}>
-                  <input type="radio" name="top_clutcher" value="minimum"/> <label htmlFor=""> Minimum</label>
-            <input type="radio" name="top_clutcher" value="range" /> <label>range</label>
-               </section>
-           </div>
+               
+                  <input type="radio"  onChange={eventHandle3} name="top_clutcher" value="minimum"/> <label htmlFor=""> Minimum</label>
+            <input type="radio"  onChange={eventHandle3} name="top_clutcher" value="range" /> <label>range</label>
+               
+        
           </div>
             </div>
             <div className="mvp">
            < input type="CheckBox"   onClick= {()=>{setsniper(s => !s)}}/>
             <p>Top Sniper</p>
-            <div className="card" style={ {display:sniper? "block ":"none" }}>
+            <div  style={ {display:sniper? "block ":"none" }}>
           <input type="text" placeholder="Numder of Prizes" />
             <input type="text" placeholder="prize amount" />
-           <div className="condition">
+         
              {/* radio btn section*/}
-               <section onChange={eventHandle3}>
-                  <input type="radio" name="top_sniper" value="minimum"/> <label htmlFor=""> Minimum</label>
-            <input type="radio" name="top_sniper" value="range" /> <label>range</label>
-               </section>
-           </div>
+                  <input type="radio"  onChange={eventHandle3} name="top_sniper" value="minimum"/> <label htmlFor=""> Minimum</label>
+            <input type="radio"  onChange={eventHandle3} name="top_sniper" value="range" /> <label>range</label>
+     
           </div>
             </div>
+            
         </div>
         <br />
         
-        </div>
+        
+        
+        </>
     )
 }
 
